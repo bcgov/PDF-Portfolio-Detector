6c2f1132fb165b315e1cc360cf45fcbd1242759d
LTU-AR <Aidan.Richards@gov.bc.ca>
2024-09-19 12:21:05 -0700

    test

--- PDFDetector.py
+++ PDFDetector.py
@@ -10,7 +10,7 @@ root.attributes('-alpha',0.5) #transparent window
 def popup_msg():
     msg = messagebox.showinfo("Result", "Process Finished")
 
-
+ 
 button = Button(root, text = 'Click me !', command = popup_msg)
 # button.pack(side = 'middle') #makes button position be at the top
 button.place(relx=0.5, rely=0.5, anchor=CENTER)
