ef2a9ac2e9451b12b0af093d62154f360ac329c3
Kevin Wai <kevin.h.wai@gov.bc.ca>
2024-09-19 11:55:07 -0700

    Start template

---
+++ PDFDetector.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter import messagebox
+import tkinter
+
+root = Tk()
+root.geometry("900x600") #default size when opened
+root.configure(background='cyan') #background color
+root.attributes('-alpha',0.5) #transparent window
+
+def popup_msg():
+    msg = messagebox.showinfo("Result", "Process Finished")
+
+
+button = Button(root, text = 'Click me !', command = popup_msg)
+# button.pack(side = 'middle') #makes button position be at the top
+button.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+root.mainloop()
